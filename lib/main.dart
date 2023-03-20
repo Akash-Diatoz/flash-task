3bc6d42fa7760618f4c1f266e71aa5dba613f346
Akash701 <akashjnair701@gmail.com>
2023-03-20 17:07:57 +0530

    Notifier changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:firetask/application/firebase_analytics.dart';
 import 'package:firetask/infrastructure/locator.dart';
+import 'package:firetask/presentation/LoginPage/googleSignin.dart';
 import 'package:firetask/presentation/LoginPage/login_page.dart';
 import 'package:firetask/presentation/HomeScreen/main_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'core/colors.dart';
 import 'core/constants.dart';
 import 'firebase_options.dart';
@@ -30,25 +32,25 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        navigatorKey: navigatorKey,
-        navigatorObservers: [
-          locator<AnalyticsService>().getAnalyticsObserver()
-        ],
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(color: kBlackColor),
-          scaffoldBackgroundColor: backgroundColor,
-          primarySwatch: Colors.blue,
-          backgroundColor: Colors.black,
-          textTheme: const TextTheme(
-            bodyText1: TextStyle(color: Colors.white),
-            bodyText2: TextStyle(color: Colors.white),
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+          navigatorKey: navigatorKey,
+          navigatorObservers: [
+            locator<AnalyticsService>().getAnalyticsObserver()
+          ],
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(color: kBlackColor),
+            scaffoldBackgroundColor: backgroundColor,
+            primarySwatch: Colors.blue,
+            backgroundColor: Colors.black,
+            textTheme: const TextTheme(
+              bodyText1: TextStyle(color: Colors.white),
+              bodyText2: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-        home: MainPage());
-  }
+          home: MainPage()));
 }
 
 class MainPage extends StatelessWidget {
